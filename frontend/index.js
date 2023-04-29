33d2919c8e28d3382418496353d29eb755746290
Janice Chen <janicechen31997@hotmail.com>
2023-04-29 13:10:59 -0500

    actu

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import Music from "./music";
 
-const index = () => {
-    
-  return (
-    <div>
-      
-    </div>
-  )
+function Music(){
+    const [info, setInfo] = useState([])
+    const [endpoint, setBusqueda] = useState("")
+
+     const getData = async() => {
+    const url =`https://api.spotify.com/${endpoint}`
+    const resp = await axios.get(url)
+    setInfo(resp.data.data)
+    console.log(resp.data.data)
+    }
+
+    useEffect(() =>{
+        getData(); 
+    },[])
+
+    const handleInput = (e) =>{
+        setBusqueda(e.target.value)
+        console.log(e.target.value)
+    }
+
+    const handleSubmit  = () =>{
+    getData()
+    setBusqueda('')
+    }
+
+    return(
+        <div>
+            <h1>La musica</h1>
+
+            <input type="text" onChange={handleInput}/>
+            <button onClick={handleSubmit}>Enviar</button>
+
+            <Dataghf info={info}/>
+        </div>
+    )
 }
 
-export default index
+export default Music
